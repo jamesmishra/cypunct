4ec22bafb3a6732754b6f97b43ba75736d89656f
James Mishra <james@anton.ai>
2017-07-03 15:43:28 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
+import io
+import os
 from setuptools import Extension
 from setuptools import setup
 
+version = '0.1.0'  # pylint: disable=invalid-name
+
+DESCRIPTION = """Cypunct is a Cython package to split Unicode
+strings based on a given frozenset of Unicode code points."""
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(ROOT, 'README.rst'), encoding='utf8') as f:
+    README = f.read()
+
 setup(
     name="cypunct",
+    description=DESCRIPTION,
+    long_description=README,
+    author="James Mishra",
+    author_email="j@jamesmishra.com",
+    url="https://github.com/jamesmishra/cypunct",
+    license='MIT',
+    version=version,
     packages=["cypunct"],
     py_modules=[
         "cypunct.cypunct",
@@ -18,6 +36,7 @@ setup(
         # Setuptools 18.0 properly handles Cython extensions.
         'setuptools>=18.0',
         'cython',
+        "zest.releaser"
     ],
     tests_require=[
         # We compare our speed benchmarks to a Unicode regex
@@ -38,5 +57,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
-    ]
+    ],
+    zip_safe=False
 )
