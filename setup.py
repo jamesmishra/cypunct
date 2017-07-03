8b7427f28003a278117c41e1c1e37f8e838811fb
James Mishra <james@anton.ai>
2017-07-03 16:02:16 -0700

    Preparing release 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import Extension
 from setuptools import setup
 
-version = '0.1.1.dev0'
+version = '0.1.1'
 
 DESCRIPTION = (
     "Cypunct is a Cython package to split Unicode "
